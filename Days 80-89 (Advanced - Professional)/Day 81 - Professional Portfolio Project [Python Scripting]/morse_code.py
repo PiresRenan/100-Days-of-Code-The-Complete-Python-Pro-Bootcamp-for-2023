6a032ec04e73618a600f869b8ff9d543d6c207ca
Renan Pires <renan.sp.121@hotmail.com>
2023-02-14 11:35:32 -0300

    [14/02/2023]

---
+++ Days 80-89 (Advanced - Professional)/Day 81 - Professional Portfolio Project [Python Scripting]/morse_code.py
@@ -0,0 +1,13 @@
+morse_code_dict = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----'}
+
+
+def string_to_morse_code(string):
+    morse_code = ''
+    for char in string.upper():
+        if char in morse_code_dict:
+            morse_code += morse_code_dict[char] + ' '
+        else:
+            morse_code += char + ' '
+    return morse_code.strip()
+
+print(string_to_morse_code('Hello World!'))
